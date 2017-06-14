349d1e1531cb6fc4f57e637c01aaa98a746a686b
Julio Berina <julioberina@gmail.com>
2017-06-14 22:07:06 +0000

    Added permissions to lessons controller so only enrolled users can view content

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,17 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrolled_for_lesson_viewing, only: [:show]
+
   def show
   end
 
   private
+
+  def require_enrolled_for_lesson_viewing
+    unless current_user.enrolled_in? current_lesson.section.course
+      redirect_to course_path(current_lesson.section.course), alert: 'You must be enrolled in the course to view lesson'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
